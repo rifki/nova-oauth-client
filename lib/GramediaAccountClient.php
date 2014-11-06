599359e7b6a34ea9e0054f11c696f8d91b9558e8
Muhamad Rifki <rifki.linux@gmail.com>
2014-11-06 22:00:54 +0700

    fixed

--- lib/GramediaAccountClient.php
+++ lib/GramediaAccountClient.php
@@ -1,27 +1,109 @@
 <?php
-if (!function_exists('curl_init')) { throw new Exception('GOM Account needs the CURL PHP extension.'); }
-if (!function_exists('json_decode')) { throw new Exception('GOM Account needs the JSON PHP extension.'); }
+if (!function_exists('curl_init')) { throw new Exception('GramediaAccountClient needs the CURL PHP extension.'); }
+if (!function_exists('json_decode')) { throw new Exception('GramediaAccountClient needs the JSON PHP extension.'); }
 
 /**
- * GOM Account Oauth2 Client
- * @author Muhamad Rifki
- * @version 1.0.0
- */
-class GramediaAccountClient {
-	const VERSION 				= '1.0.0';
-	protected $useragent 		= 'gom-account-oauth-1.0';
-	protected $connecttimeout 	= 30;
-	protected $timeout 			= 30;
-	protected $ssl_verifypeer 	= false; 
-	protected $host 			= 'http://localhost/projects/gom-server-sdk'; // URL Host Server
+* GOM Account Oauth2 Client
+*
+* @author Muhamad Rifki
+* @copyright Digital Media - Kompas Gramedia Group of Magazine
+* @version 1.0.0
+*/
+class GramediaAccountClient 
+{
+	# current version 
+	const VERSION = '1.0.0';
+
+	# user agent
+	protected $useragent = 'gom-account-oauth-1.0';
+
+	# connection timeout
+	protected $connecttimeout = 30;
+
+	# timeout
+	protected $timeout = 30;
+
+	# ssl verifypeer
+	protected $ssl_verifypeer = false; 
+
+	# URL Host Server
+	protected $server;
+
+	# client ID
+	protected $clientId;
+
+	# client secret
+	protected $clientSecret;
+
+	# callback
+	protected $callback;
+
+	/**
+	* Initialize a Gramedia Account Application.
+	*
+	* The configuration (Mandatory):
+	* 	- server: the server host
+	* 	- clientId: the client id
+	* 	- clientSecret: the client secret
+	* 	- callback: the callback URL
+	*
+	* @param array $config The application configuration
+	*/
+	public function __construct($config) {
+		if (function_exists('session_status') && 
+	    	session_status() !== PHP_SESSION_ACTIVE || 
+	    	!session_id()) {
+	    	session_start();
+	    }
+
+	    $this->setServer($config['server']);
+	    $this->setClientId($config['clientId']);
+	    $this->setClientSecret($config['clientSecret']);
+	    $this->setCallback($config['callback']);
+	}
+
+	/**
+	* Get the getResponseType.
+	*
+	* @return string type of response
+	*/
+	public function getResponseType() {
+		return 'code';
+	}
 
    /**
 	* Set API URLs
 	*/
-	public function authorizeURL()		 { return $this->host.'/oauth/authorize'; }
-	public function tokenURL() 			 { return $this->host.'/oauth/access_token'; }
-	public function resourceUserURL()	 { return $this->host.'/user'; }
-	public function resourceEntriesURL() { return $this->host.'/email'; }
+	public function authorizeURL() { 
+		return $this->getServer().'/oauth/authorize'; 
+	}
+	
+	/**
+	* Get the Token URL.
+	*
+	* @return string 
+	*/
+	public function tokenURL() { 
+		return $this->getServer().'/oauth/access_token'; 
+	}
+	
+	/**
+	* Get the Resource User URL.
+	*
+	* @return string 
+	*/
+	public function resourceUserURL() { 
+		return $this->getServer().'/user'; 
+	}
+	
+	/**
+	* Get the Resource Entries URL.
+	*
+	* @return string 
+	*/
+	public function resourceEntriesURL() { 
+		return $this->getServer().'/email'; 
+	}
 
    /**
 	* Makes an HTTP request.
@@ -55,55 +137,144 @@ class GramediaAccountClient {
 		curl_close($ch);
 		return json_decode($response);
 	}
- 
+ 	
+ 	/**
+	* Get parameter via the URL
+	*
+	* @return bool
+	*/
 	public function get($key, $default=null) 
 	{
 		return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
 	}
- 
+ 	
+ 	/**
+	* Get sesssion key
+	*
+	* @return bool
+	*/
 	public function session($key, $default=null) 
 	{
 		return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
 	}
 
+	/**
+	* Generate Randomly for state
+	*
+	* @return string
+	*/
 	public function getState() 
 	{ 
 		return hash('sha256', microtime(true).rand());
 	}	
-}
 
-/**
- * Helper for Iframe and Javascript popup fancybox
- */
-class Helper {
-   /**
-	* Fancybox popup
+	/**
+	* The host server oauth2
 	*
-	* @param bool $is_local The asset fancybox
-	* @param string $class_iframe name of class iframe (for event javascript default gom_iframe)
+	* @param string $appId The host server
+	*
+	* @return GramediaAccountClient
+	*/
+	public function setServer($server) {
+		$this->server = $server;
+		return $this;
+	}
+
+	/**
+	* Get the Host Server OAuth2.
+	*
+	* @return string the Host Server
+	*/
+	public function getServer() {
+		return $this->server;
+	}
+
+	/**
+	* Set the Client ID.
+	*
+	* @param string $clientId The Client ID
+	*
+	* @return GramediaAccountClient
+	*/
+	public function setClientId($clientId) {
+		$this->clientId = $clientId;
+		return $this;
+	}
+
+	/**
+	* Get the Client ID.
+	*
+	* @return string the Client ID
+	*/
+	public function getClientId() {
+		return $this->clientId;
+	}
+
+	/**
+	* Set the Client Secret.
+	*
+	* @param string $clientSecret The Client Secret
+	*
+	* @return GramediaAccountClient
+	*/
+	public function setClientSecret($clientSecret) {
+		$this->clientSecret = $clientSecret;
+		return $this;
+	}
+
+	/**
+	* Get the Client Secret.
+	*
+	* @return string the Client Secret
+	*/
+	public function getClientSecret() {
+		return $this->clientSecret;
+	}
+
+	/**
+	* Set the Callback URL.
+	*
+	* @param string $callback The Callback URL
+	*
+	* @return GramediaAccountClient
+	*/
+	public function setCallback($callback) {
+		$this->callback = $callback;
+	}
+
+	/**
+	* Get the Callback URL.
+	*
+	* @return string the callback URL
+	*/
+	public function getCallback() {
+		return $this->callback;
+	}
+
+	/**
+	* Asset Fancybox
 	*
 	* @return string
 	*/
-	public static function showPopup($is_local=true) 
+	public static function asset($width = 400, $height = 400) 
 	{
 		$js = "
+			<script type='text/javascript' src='".asset('assets/fancybox/jquery.fancybox.pack.js')."'></script>
+			<link rel='stylesheet' type='text/css' href='".asset('assets/fancybox/jquery.fancybox.css')."' media='screen' />
 			<script type='text/javascript'>
 			    $(function() {
 			        $('.gom_oauth_iframe').fancybox({
 			            type: 'iframe',
 			            autoDimensions: false,
-			            width: 400,
-			            height:400,
+			            width: ".$width.",
+			            height: ".$height.",
 			            overlayShow: true,
 			            speedIn: 500, 
 			            speedOut: 500,
 			            transitionIn: 'elastic',
 			            transitionOut: 'elastic',
-			            afterLoad: function() {
-			                //$('#gom_iframe').text('Please wait...');
-			            },
 			            afterClose: function () {
-			                location = document.URL;
+			                location.reload()
 			            }
 			        });
 			    });
@@ -114,17 +285,20 @@ class Helper {
 
    /**
 	* Display link iframe
+	* 
+	* Configuration (Mandatory):
+	* - required add class 'gom_oauth_iframe' in link
+	* example <a href='#' class='gom_oauth_iframe'>Login</a>
 	*
-	* @param string $class_iframe name of class iframe (default gom_iframe)
-	* @param string callback
-	*
+	* @param string callback URL
 	* @return string
 	*/
-	public static function showLink($callback)
+	public static function link($callback)
 	{
 		if (!empty($callback)) {
-			return "<a class='gom_oauth_iframe' href='".$callback."'>Gramedia Account Connect</a>";	
-		} else {
+			return $callback;
+		} 
+		else {
 			return false;
 		}
 	}
